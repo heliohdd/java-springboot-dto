9424196cda053d175e7725bb9fb00ef6b2e213d8
Helio Dourado <heliohdd@yahoo.com.br>
2024-04-05 13:58:56 -0300

    Created project with DB H2 and JPA dependencies

---
+++ src/main/java/com/algaworks/javaspringbootdto/JavaSpringbootDtoApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.javaspringbootdto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootDtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/javaspringbootdto/JavaSpringbootDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.javaspringbootdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
